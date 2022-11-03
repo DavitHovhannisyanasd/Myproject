ae0c42ea7a44ab42b3853c7f943c21d239c65bbb
DavitHovhannisyanasd <davit.h74.y@tumo.org>
2022-11-03 16:46:50 +0400

    asd

--- prog3/grass.js
+++ prog3/grass.js
@@ -7,8 +7,8 @@ class Grass {
 
         this.directions = [
             [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
+            [this.x, this.y -  1],
+            [this.x + 1, this.y  - 1],
             [this.x - 1, this.y],
             [this.x + 1, this.y],
             [this.x - 1, this.y + 1],
