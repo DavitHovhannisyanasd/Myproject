2905976cbd6bfc958c3d7f7500f43810cc0dfdb1
Serjik07 <serjik.h1@tumo.org>
2022-11-06 12:20:01 +0400

    տեքստ այն մասին, թե ինչ փոփոխություններ են արվել

--- prog3/predator.js
+++ prog3/predator.js
@@ -1,35 +1,12 @@
-class Predator {
+class Predator extends LivingCreature {
     constructor(x, y, index) {
-        this.x = x;
-        this.y = y;
-        this.index = index;
+        super(x, y, index);
         this.time = 10;
         this.energy = 3;
     }
-    getNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    }
-    chooseCell(character) {
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
+
+    chooseCell(ch) {
+        return super.chooseCell(ch)
     }
     move() {
         this.getNewCoordinates();
